710cd627c4266b48a6d45749a501eeaab04b74a2
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-03-22 02:46:09 +0300

    Update a_b_c_counter.py

--- Лаб1/code/a_b_c_counter.py
+++ Лаб1/code/a_b_c_counter.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 my_sigma = 1000  # мелкость разбиения
+
+
 def fun_square_wave(t):
     if (t // np.pi) % 2 == 0:
         return 2
@@ -15,30 +17,46 @@ fun_even = lambda t: 4 * np.cos(np.sin(t / 2) ** 2)
 fun_odd = lambda t: np.sin(t) ** 3
 
 
-
 #def fun_not_odd_not_even(t):
-
 
 # here we count integral of two fun mult
 def integral_counter(f_1, f_2, a, b):
 
     t = np.linspace(a, b, my_sigma)
-    dt = (a - b) / my_sigma
+    dt = (b - a) / my_sigma
 
     return np.dot(f_1(t), f_2(t)) * dt
 
 
 def a_coef(fun, N):
-    a = []
+    res = []
     for n in range(N):
         fourier_cos = lambda t: np.cos(n * t)
-        a.append(integral_counter(fun, fourier_cos, -np.pi, np.pi) / np.pi)
+        res.append(integral_counter(fun, fourier_cos, -np.pi, np.pi) / np.pi)
 
-    return a
+    return res
+
+
+def b_coef(fun, N):
+    res = []
+    for n in range(N):
+        fourier_sin = lambda t: np.sin(n * t)
+        res.append(integral_counter(fun, fourier_sin, -np.pi, np.pi) / np.pi)
+    return res
+
+
+def c_coef(fun, N):
+    res = []
+    for n in range(-N, N):
+        fourier_exp = lambda t: np.exp(-1j * n * t)
+        res.append(integral_counter(fun, fourier_exp, -np.pi, np.pi) / (2 * np.pi))
+    return res
 
 
 def main():
     print(a_coef(fun_even, 3))
+    print(b_coef(fun_even, 5))
+    print(c_coef(fun_even, 5))
 
 
 main()
