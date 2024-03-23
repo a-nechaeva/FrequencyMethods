c7af12eff25cc2cb68341b657b3edf2d11402e0c
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-03-23 23:39:05 +0300

    frec

--- Лаб1/code/a_b_c_counter.py
+++ Лаб1/code/a_b_c_counter.py
@@ -2,7 +2,7 @@ import scipy.integrate as integrate
 import numpy as np
 
 
-my_sigma = 1000  # мелкость разбиения
+my_sigma = 10000  # мелкость разбиения
 
 
 def fun_square_wave(t):
@@ -11,6 +11,7 @@ def fun_square_wave(t):
     else:
         return 1
 
+fun_wave = np.vectorize(fun_square_wave)
 
 fun_even = lambda t: 4 * np.cos(np.sin(t / 2) ** 2)
 
@@ -28,35 +29,41 @@ def integral_counter(f_1, f_2, a, b):
     return np.dot(f_1(t), f_2(t)) * dt
 
 
-def a_coef(fun, N):
+def a_coef(f, N):
     res = []
     for n in range(N):
         fourier_cos = lambda t: np.cos(n * t)
-        res.append(integral_counter(fun, fourier_cos, -np.pi, np.pi) / np.pi)
+        res.append(integral_counter(f, fourier_cos, -np.pi, np.pi) / np.pi)
 
     return res
 
 
-def b_coef(fun, N):
+def b_coef(f, N):
     res = []
     for n in range(N):
         fourier_sin = lambda t: np.sin(n * t)
-        res.append(integral_counter(fun, fourier_sin, -np.pi, np.pi) / np.pi)
+        res.append(integral_counter(f, fourier_sin, -np.pi, np.pi) / np.pi)
     return res
 
 
-def c_coef(fun, N):
+def c_coef(f, N):
     res = []
     for n in range(-N, N):
         fourier_exp = lambda t: np.exp(-1j * n * t)
-        res.append(integral_counter(fun, fourier_exp, -np.pi, np.pi) / (2 * np.pi))
+        res.append(integral_counter(f, fourier_exp, -np.pi, np.pi) / (2 * np.pi))
     return res
 
 
 def main():
-    print(a_coef(fun_even, 3))
-    print(b_coef(fun_even, 5))
-    print(c_coef(fun_even, 5))
+    N = 3
+    a = a_coef(fun_wave, N)
+    b = b_coef(fun_wave, N)
+    c = c_coef(fun_wave, N)
+
+    for n in range(N):
+        print("a_", n, " = ", a[n], "    ", "b_", n, " = ", b[n],  "\n")
+
+    print(c_coef(fun_wave, 3))
 
 
 main()
