74f1487c0f5daa49f7fa5b109f7c6eb73d29e666
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-04-01 16:51:22 +0300

    upd

--- Лаб1/code/parseval_check.py
+++ Лаб1/code/parseval_check.py
@@ -1,9 +1,23 @@
 from a_b_c_counter import *
 
+
+#  parseval for F
 def parseval_F(f, N):
 
     a = a_coef(f, N)
     b = b_coef(f, N)
-    res = 
+    res = a[0] ** 2 / 2
 
-    for n in range(N):
+    for n in range(1, N):
+        res += a[n] ** 2 + b[n] ** 2
+
+    return np.pi * res
+
+
+#  parseval for G
+def parseval_G(f, N):
+
+    c = c_coef(f, N)
+
+    for n in range(len(c)):
+        
