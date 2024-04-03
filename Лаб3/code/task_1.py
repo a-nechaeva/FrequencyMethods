062b5c5355bf5c28f7dd435969b6ffcfa4d49461
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-04-03 16:00:06 +0300

    Update task_1.py

--- Лаб3/code/task_1.py
+++ Лаб3/code/task_1.py
@@ -12,13 +12,12 @@ def _g(a, t_1, t_2, t):
 
 
 _g_vec = np.vectorize(_g)
+
+
 #  draw graphic of original function
-def _draw_g():
-    a = 1
-    t_1 = 0
-    t_2 = 2
-    T = 20
-    t = np.linspace(- T / 2, T / 2, 1000)
+def _draw_g(a, t_1, t_2, T, _sigma):
+
+    t = np.linspace(- T / 2, T / 2, _sigma)
 
     plt.plot(t, _g_vec(a, t_1, t_2, t), label='g(t)')
     plt.legend()
@@ -27,9 +26,38 @@ def _draw_g():
     plt.show()
 
 
+#  noise function
+_noise_fun = lambda a, b, c, d, t_1, t_2, t:  (_g_vec(a, t_1, t_2, t) + b * (np.random.rand(t.size) - 0.5)
+                                               + c * np.sin(d * t))
+
+
+#  draw graphic of noisy function
+def _draw_noise(a, b, c, d, t_1, t_2, T, _sigma):
+
+    t = np.linspace(- T / 2, T / 2, _sigma)
+
+    plt.plot(t, _noise_fun(a, b, c, d, t_1, t_2, t), label='g(t) with noise')
+    plt.legend()
+    plt.grid()
+    plt.title('Зашумленный график исходной функции g(t)')
+    plt.show()
+
+
+
 #  run function
 def _run():
-    _draw_g()
+    a = 1
+    b = 0.5
+    c = 0
+    d = 0
+
+    t_1 = 0
+    t_2 = 2
+    T = 20
+    _sigma = 1000
+
+    #  _draw_g(a, t_1, t_2, T, _sigma)
+    _draw_noise(a, b, c, d, t_1, t_2, T, _sigma)
 
 
 _run()
