a66d66a70976e0117c8f06a4f07c7bd1850018a1
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-05-02 23:57:41 +0300

    work-work-work

--- Лаб5/code/task_1.py
+++ Лаб5/code/task_1.py
@@ -6,7 +6,8 @@ import datetime
 _start = datetime.datetime.now()
 
 
-n = 30
+n = 10000
+d = 5
 #  start function
 _f = lambda t: 1 if abs(t) <= 0.5 else 0
 
@@ -25,13 +26,15 @@ _fourier_fun = lambda im, x, v: np.array([int_c(v, im, (lambda t: np.e ** (1j * 
 
 
 def _draw_graph_im(f, t, title):
-
+    new_t = np.linspace(-d, d, 5000)
     #plt.plot(t, f(t), label=r'$\hat{\Pi}(\nu)$', color='midnightblue')
-    #plt.plot(t, f, label=r'$\hat{\Pi}(\nu)$', color='midnightblue')
-    plt.plot(t, f, label=r'$\Pi(t)$', color='midnightblue')
-    plt.xlabel(r'$t$')
-    #plt.ylabel(r'$\hat{\Pi}(\nu)$')
-    plt.ylabel(r'$\Pi(t)$')
+    plt.plot(t, f, label=r'восстановленный', color='darkmagenta')
+    plt.plot(new_t, _f_image(new_t), label=r'исходный', color='midnightblue')
+    #plt.plot(t, f, label=r'$\Pi(t)$', color='midnightblue')
+    plt.xlabel(r'$\nu$')
+    #plt.xlabel(r'$t$')
+    plt.ylabel(r'$\hat{\Pi}(\nu)$')
+    #plt.ylabel(r'$\Pi(t)$')
     plt.grid()
     plt.legend()
     plt.title(title)
@@ -39,7 +42,7 @@ def _draw_graph_im(f, t, title):
 
 
 def _draw_graph_f(f, t, title):
-    new_t = np.linspace(-5, 5, 5000)
+    new_t = np.linspace(-d, d, 5000)
     plt.plot(new_t,  _vf(new_t), label=r'$\Pi(t)$', color='midnightblue')
     plt.plot(t, f, label=r'восстановленная $\Pi(t)$', color='darkmagenta')
     plt.xlabel(r'$t$')
@@ -52,13 +55,13 @@ def _draw_graph_f(f, t, title):
 
 
 
-time = np.linspace(-5, 5, n)
-_v = np.linspace(-5, 5, n)
+time = np.linspace(-d, d, n)
+_v = np.linspace(-d, d, n)
 
 #  _draw_graph(_vf, time, 'График исходной функции')
 #_draw_graph(_f_image, time, 'График Фурье-образа')
-#_draw_graph(_fourier_image(_vf(time), time, time), time, 'График Фурье-образа')
-_draw_graph_f(_fourier_fun(_fourier_image(_vf(time), time, _v), time, _v), time, r'Графики функций при $n = $' + str(n))
+_draw_graph_im(_fourier_image(_vf(time), time, time), time, r'График Фурье-образа при $n = $' + str(n))
+# _draw_graph_f(_fourier_fun(_fourier_image(_vf(time), time, _v), time, _v), time, r'Графики функций при $n = $' + str(n))
 
 
 _finish = datetime.datetime.now()
